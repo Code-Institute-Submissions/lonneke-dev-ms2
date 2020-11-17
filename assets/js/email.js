7af7e5e1d25a4a61a2e728f63064905a15e760ff
lonneke-dev <lonneke1908@outlook.com>
2020-11-17 14:05:12 +0000

    Added an error and succes message to the email form

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,15 +2,39 @@ function sendMail(contactForm) {
     emailjs.send("service_gyau47p","lonneke", {
         "from_name" : contactForm.name.value,
         "from_email" : contactForm.emailaddress.value,
-        "contact" : contactForm.contactinfo.value
+        "message" : contactForm.message.value
     })
     .then(
         function(response) {
             console.log("SUCCES", response);
+            emailSend();
         },
         function(error) {
             console.log("FAILED", error);
+            emailError();
         }
     );
     return false;
+}
+
+const emailBox = document.querySelector(".email-box");
+const formBox = document.querySelector(".form-box");
+const errorBox = document.querySelector(".error-box");
+
+function emailSend() {
+    emailBox.classList.remove("hide");
+
+    formBox.classList.add("hide");
+}
+
+function emailError() {
+    errorBox.classList.remove("hide");
+
+    formBox.classList.add("hide");
+}
+
+function GoBackToForm() {
+    formBox.classList.remove("hide");
+
+    errorBox.classList.add("hide");
 }
