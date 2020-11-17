07fcc5f41dfa17c61f7f7b735cdb371a4cd47034
lonneke-dev <lonneke1908@outlook.com>
2020-11-17 15:10:08 +0000

    Made seperate js files

---
+++ assets/js/instructions.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('instructionsModal');
+var button = document.getElementById('instructions');
+var span = document.getElementsByClassName('close')[0];
+
+button.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
